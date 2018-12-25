35bdbff98a3d54dd9a4f7cf9ece28169f54ac740
Wouter Groen <wa.groen@gmail.com>
2018-12-25 14:14:30 +0100

    Added a seeder for the users table

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder {
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        self::addUserIfNotExists('admin', 'admin@nichesolutions.nl', 'admin');
+    }
+    
+    public static function addUserIfNotExists($name, $email, $pass) {
+        if (!User::where('name', $name)->exists()) {
+            User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => Hash::make($pass),
+            ]);
+        }
+    }
+}
